07d7291f3b597f8263e6596ad5fff247a141f0c5
Jasmine <jcare011@odu.edu>
2022-07-11 17:15:37 -0400

    adding files to required package

--- src/main/java/edu/odu/cs/cs350/ExcelWriter.java
+++ src/main/java/edu/odu/cs/cs350/ExcelWriter.java
@@ -1,4 +1,4 @@
-
+package edu.odu.cs.cs350;
 import java.io.*;
 import java.util.*;
 //import java.io.IOException;
--- src/main/java/edu/odu/cs/cs350/Website.java
+++ src/main/java/edu/odu/cs/cs350/Website.java
@@ -1,4 +1,4 @@
-package src.main.java;
+package edu.odu.cs.cs350;
 class Website {
 
 public static void main(String[] args)
