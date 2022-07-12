6a0e277439a855fe7973ac8dccd965ff28b1135f
rmack1020 <rmack005@odu.edu>
2022-07-11 23:11:58 -0400

    CLI update

--- src/main/java/edu/odu/cs/cs350/CommandLine.java
+++ src/main/java/edu/odu/cs/cs350/CommandLine.java
@@ -1,1 +1,31 @@
-package edu.odu.cs.cs350;
+package src.main.java;
+public class CommandLine {
+    
+    public void main(String args[])
+    {
+        //Example File List
+        String[] fileList = {"test.txt", "test.xlxs", "test.json"};
+        //Example File Size 
+        int FileListSize = 3;
+        outPutFileNames(fileList, FileListSize);
+    }
+    //Function will access files and pull names
+    public static String fetchFileNames(String[] FileList)
+    {
+        
+    }
+    //Function will access files and collect number of files in total
+    public static String fetchNumberFiles(int numFiles)
+    {
+
+    }
+    //Function will output file names to CLI
+    public static void outPutFileNames(String[] FileList, int FileListSize)
+    {
+        for (int i = 0; i < FileListSize; i++)
+        {
+            System.out.println(FileList);
+        }
+    }
+    
+}
