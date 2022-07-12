a5497812f6dd9cb845001547d778687fe45b249d
rmack1020 <rmack005@odu.edu>
2022-07-11 22:22:03 -0400

    CLI Test File added

---
+++ src/test/testCommandLineInterface.java
@@ -0,0 +1,8 @@
+package src.test;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+public class testCommandLineInterface {
+    
+}
