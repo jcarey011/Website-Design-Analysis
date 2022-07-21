932f65a8cedfeef69fc26866fea866782818f313
Joel Lavinder <jlavi001@odu.edu>
2022-07-21 16:49:22 -0400

    named json output file

--- src/main/java/edu/odu/cs/cs350/JsonWriter.java
+++ src/main/java/edu/odu/cs/cs350/JsonWriter.java
@@ -2,6 +2,8 @@ package edu.odu.cs.cs350;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import org.json.simple.JSONObject;
 //import org.json.simple.JSONArray;
@@ -9,13 +11,29 @@ import org.json.simple.JSONObject;
 
 
 
-public class JsonWriter {	
+public class JsonWriter {
 	public static void main(String args[]) {
+		CreateJson();
+	}
+	/*
+	 *	Produces filename (duplicate function used in ExcelWriter; authored by Jasmine) 
+	 */
+	public static String produceFileName(){
+		LocalDateTime today = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-hhmmss");
+		String formattedTime = formatter.format(today);
+
+		return formattedTime + "-summary.json";
+	}
+	/*
+	 * Create and output json file
+	 */
+	public static void CreateJson() {
 		JSONObject jObject  = new JSONObject();
-		jObject.put("Test", "This is a Test");
+		jObject.put("Images", "Image Information");
 
 		try {
-			FileWriter file = new FileWriter("test.json");
+			FileWriter file = new FileWriter(produceFileName());
 			file.write(jObject.toJSONString());
 			file.close();
 		} 
