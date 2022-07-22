f6cbcd4070b46cb783a94c4c7d02c3169acdfcdb
hhawa01 <hhawa001@odu.edu>
2022-07-22 12:18:25 -0400

    write to txt file and totalize sizes

--- src/main/java/edu/odu/cs/cs350/TextWriter.java
+++ src/main/java/edu/odu/cs/cs350/TextWriter.java
@@ -1,4 +1,9 @@
 package edu.odu.cs.cs350;
+import java.io.File;
+import java.io.PrintStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 
 
 public class TextWriter
@@ -7,15 +12,40 @@ public class TextWriter
     {
 
     //string array with fake directory data 
-    String[] directory = {"Rainbow", "Reindeer", "Apple", "Zebra", "Unicorn", "Test"};
-    
+    String[] directory = {"/Rainbow", "/Reindeer", "/Apple", "/Zebra", "/Unicorn", "/Test"};
+    int[] sizes = {125, 657, 27, 195, 206, 987};
     //calling method to sort directory names lexicographically
     sortingDirectories(directory);
 
-    //printing to commandline output to visualize/test
+    
+    int i=0;
+    int total=0;
+    String name= FileName();
+    //creating txt file 
+    try{
+        File file = new File (name);
+        PrintStream fileStream = new PrintStream(file);
+        System.setOut(fileStream);
+    }
+    catch (Exception e)
+    {
+        System.out.println(e);
+    }
+   
+    //printing out to newly created txt file
     for(String string : directory)
-        System.out.print(string + " ");
-    System.out.println();
+    {
+        System.out.print(sizes[i] + " ");
+        System.out.println("." + string);
+        total=sizes[i]+total;
+        i++;
+    }
+    System.out.println(total); 
+
+
+
+
+    
 
 }
 //method to lexicographically sort directory names
@@ -32,6 +62,18 @@ public static String[] sortingDirectories(String[] directories)
     }
     return directories;
 }
+
+
+
+//method to get filename as current day
+public static String FileName(){
+
+    LocalDateTime today = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-hhmmss");
+        String formattedTime = formatter.format(today);
+
+        return formattedTime + "-summary.txt";
 }
 
+}
 
