13529c8b426181a42bc2da3a0e7911b9577a89bc
cs_tan006 <mtan006@odu.edu>
2022-07-28 22:31:26 -0400

    webtest passed

--- src/test/java/edu/odu/cs/cs350/WebTest.java
+++ src/test/java/edu/odu/cs/cs350/WebTest.java
@@ -8,26 +8,26 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
-/*public class WebTest{
+public class WebTest{
     @Test void testConstructor(){
-        String [] thingy = new String[]{"1", "settings.ini", "src/test/data/outline.html", "src/test/data/outline.htm"};
+        String [] thingy = new String[]{"1", "settings.ini", "src/test/data/outline.html"};
         List<File> files = new ArrayList<>();
         File f1 = new File("src/test/data/outline.html");
-        File f2 = new File("src/test/data/outline.htm");
+        //File f2 = new File("src/test/data/outline.htm");
         files.add(f1);
-        files.add(f2);
+       // files.add(f2);
 
-        WebTest a = new WebTest(thingy);
+        Website a = new Website(thingy);
         assertEquals(files,a.getSourceFiles());
     }
 
     @Test void testFindFilesInDir(){
         String path = "src/test/data/";
         File dir = new File(path);
-        WebTest input = new Webtest();
+        Website input = new Website();
         Options options = new Options();
         List<File> files = input.findFilesInDir(dir, options);
 
         assertEquals(5, files.size());
     }
-}*/
+}
