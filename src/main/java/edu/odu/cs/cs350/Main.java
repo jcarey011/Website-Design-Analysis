cb82565acfcbd96bf20881771667961c604e8d2c
rmack1020 <rmack005@odu.edu>
2022-08-01 14:40:28 -0400

    minor additions to main.java
    added some cli functions and imports

--- src/main/java/edu/odu/cs/cs350/Main.java
+++ src/main/java/edu/odu/cs/cs350/Main.java
@@ -1,5 +1,21 @@
 package edu.odu.cs.cs350;
+import edu.odu.cs.cs350.CommandLine;
+import edu.odu.cs.cs350.DocParser;
+import edu.odu.cs.cs350.ExcelWriter;
+import edu.odu.cs.cs350.JsonWriter;
+import edu.odu.cs.cs350.Options;
+import edu.odu.cs.cs350.TextWriter;
+import edu.odu.cs.cs350.Website;
 
 public class Main {
     
+public static void main(String args[]){
+    
+    
+    
+    
+    CommandLine.setNumberFiles(/*Number of Files*/);
+    CommandLine.outPutFileNames(CommandLine.fetchNumberFiles());
+}
+
 }
