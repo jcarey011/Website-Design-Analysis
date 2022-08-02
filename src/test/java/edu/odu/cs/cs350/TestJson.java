36c26d5f531ee99928ae2aa93b47748233218917
Joel Lavinder <jlavi001@odu.edu>
2022-08-02 12:36:57 -0400

    edit json test

--- src/test/java/edu/odu/cs/cs350/TestJson.java
+++ src/test/java/edu/odu/cs/cs350/TestJson.java
@@ -3,13 +3,13 @@ import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
 
-public class TestExcel {
+public class TestJson {
 	@Test
 	public void testJsonFileName()
 	{
 		Pattern namepat = Pattern.compile("\\d{8}-\\d{6}-summary.json");
 		string testpat = JsonWriter.produceFileName();
-		namepat.matches(testpat).matches();
+		assertThat(namepat.matches(testpat).matches());
 	}
 }
 
