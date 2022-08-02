1b9c6446b8af9144d6a72d6b2280363798e8446e
rmack1020 <rmack005@odu.edu>
2022-08-02 15:46:48 -0400

    Update TestCommandLine

--- src/test/java/edu/odu/cs/cs350/TestCommandLineInterface.java
+++ src/test/java/edu/odu/cs/cs350/TestCommandLineInterface.java
@@ -1,5 +1,5 @@
 package edu.odu.cs.cs350;
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
 import edu.odu.cs.cs350.CommandLine;
@@ -12,21 +12,21 @@ public class TestCommandLineInterface {
     {
         String fileName = "Hannibal.txt";
         CommandLine.addNames(fileName);
-        assertThat(CommandLine.fileList, contains("Hannibal.txt"));
+        assertThat(CommandLine.fileList, containsString("Hannibal.txt"));
     }
     @Test
     public static void testGetNames()
     {
         CommandLine.addNamesaddNames("Testname");
         CommandLine.getNames();
-        assertThat(CommandLine.getNames(), contains("Testname"));
+        assertThat(CommandLine.getNames(), containsString("Testname"));
     }
     @Test
     public static void testFetchFunction()
     {
         CommandLine.setNumberFiles(12);
         CommandLine.fetchNumberFiles();
-        assertThat(CommandLine.fetchNumberFiles(), contains(12));
+        assertEquals(12, CommandLine.fetchNumberFiles());
     }
 
 }
