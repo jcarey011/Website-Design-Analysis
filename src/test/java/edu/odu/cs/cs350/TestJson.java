1d058301b0632faa6bbd56a59039eed8f51f894b
Joel Lavinder <jlavi001@odu.edu>
2022-08-02 15:36:09 -0400

    removed pattern

--- src/test/java/edu/odu/cs/cs350/TestJson.java
+++ src/test/java/edu/odu/cs/cs350/TestJson.java
@@ -1,15 +1,13 @@
 package edu.odu.cs.cs350;
+
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
-
 
 public class TestJson {
 	@Test
 	public void testJsonFileName()
 	{
-		Pattern namepat = Pattern.compile("\\d{8}-\\d{6}-summary.json");
-		string testpat = JsonWriter.produceFileName();
-		assertThat(namepat.matches(testpat).matches());
+		String testpat = JsonWriter.produceFileName();
+		assertThat(testpat.matches("\\d{8}-\\d{6}-summary.json"));
 	}
 }
-
