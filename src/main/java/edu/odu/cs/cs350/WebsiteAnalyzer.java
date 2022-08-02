a13bb78d638c768abe19dafb902fde9af3014ac9
cs_tan006 <mtan006@odu.edu>
2022-08-01 22:15:52 -0400

    updates

--- src/main/java/edu/odu/cs/cs350/WebsiteAnalyzer.java
+++ src/main/java/edu/odu/cs/cs350/WebsiteAnalyzer.java
@@ -1,15 +1,21 @@
 package edu.odu.cs.cs350;
+import java.util.*;
+import java.io.*;
 
 
 public class WebsiteAnalyzer {
     
 public static void main(String args[]){
+    Website web = new Website(args);
+        Options userOptions = web.getOptions();
+        //List<*insert code*> html = new ArrayList<>();
 
-    
+        for(File file : web.getSourceFiles()){
+            println(web);
+           // *docparser* code = new *docparser*(file);
+          //  code.add(code);
+        }
 
-    
-    
-    
     
     CommandLine.setNumberFiles(CommandLine.fetchNumberFiles());
     CommandLine.outPutFileNames(CommandLine.fetchNumberFiles());
