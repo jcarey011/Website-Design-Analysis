825aa148f165e46b49378db34bfad22efb3c18d4
Jasmine <jcare011@odu.edu>
2022-08-03 16:21:27 -0400

    making edits on docparser

--- src/main/java/edu/odu/cs/cs350/DocParser.java
+++ src/main/java/edu/odu/cs/cs350/DocParser.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-import javax.swing.text.html.*;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.*;
@@ -17,90 +16,28 @@ public class DocParser
 
     public Document Parser() throws IOException{
 
-        //HTMLDocument document = new HTMLDocument(filePath);
+        
         File fileFromPath = new File("");
         Document newDoc = Jsoup.parse(fileFromPath, "null");
         return newDoc;
     }
 
-     /*private String absFilePath;
-    private List <Tag> tags;
-    private int numTags;
-
-    public DocParser(){
-        this.absFilePath = "";
-        this.tags = new ArrayList<>();
-        this.numTags = 0;
-        
-    }
-
-    public DocParser(File file) throws IOException{
-        this.absFilePath = findAbsFilePath(file);
-        this.tags = Tag(file);
-        this.numTags = this.tags.size();
-    }
-
-    public void setAbsFilePath(String filePath){
-        this.absFilePath = filePath;
-    }
-
-    public void setTags(List<Tag> tags){
-        this.tags = tags;
-    }
-
-    public void setNumTags(int n){
-        this.numTags = n;
-    }
-
-    public String getAbsFilePath(){
-        return this.absFilePath;
-    }
-
-    public List<Tag> getTags(){
-        return this.tags;
-    }
-
-    public int getNumTags(){
-        return this.numTags;
-    }
-
-    public String toString(){
-        String out = String.format("    %s, %d", this.absFilePath, this.numTags);
-        return out;
-    }
-
-    private String findAbsFilePath(File file){
-        String a = file.getAbsolutePath();
-        return a;
-    }
-
-    public List<Tag> Tag(File file) throws IOException{
-    String filePath = file.getAbsolutePath();
-    
-    ///Store these in the list and then print out the list
-
-    Document newDoc = Jsoup.parse(filePath);
-    
-    }
-    */
-
     /**
      * Extracts all anchor tags
      * 
-     * @param the parsed html file
      * 
      * @return a list of all anchor tags
      */
 
-    public List <Elements> extractAnchors(Document doc) throws IOException{
+    public List <Element> extractAnchors() throws IOException{
 
-        doc = Parser();
+        Document doc = Parser();
         Elements anchorTags = doc.select( "a[href]");
-        List<Elements> listAnchorTags = new ArrayList<Elements>();
+        List<Element> listAnchorTags = new ArrayList<>();
         
-        for (Elements e  : new ArrayList<Elements>(listAnchorTags)){
+        for (Element e  : anchorTags){
 
-            listAnchorTags.add(anchorTags);
+            listAnchorTags.add(e);
         }
         
 
@@ -111,20 +48,19 @@ public class DocParser
     /**
      * Extracts all image tags
      * 
-     * @param the parsed html file
      * 
      * @return a list of all image tags
      */
     
-    public List <Elements> extractImages(Document doc) throws IOException{
+    public List <Element> extractImages() throws IOException{
 
-        doc = Parser();
+        Document doc = Parser();
         Elements imageTags = doc.select("img[src]");
-        List<Elements> listImageTags = new ArrayList<Elements>();
+        List<Element> listImageTags = new ArrayList<>();
 
-        for (Elements e : new ArrayList<Elements>(listImageTags)){
+        for (Element e : imageTags){
 
-            listImageTags.add(imageTags);
+            listImageTags.add(e);
         }
         
 
@@ -135,20 +71,19 @@ public class DocParser
     /**
      * Extracts all link tags
      * 
-     * @param the parsed html file
      * 
      * @return a list of all link tags
      */
 
-    public List <Elements> extractLinks(Document doc) throws IOException{
+    public List <Element> extractLinks() throws IOException{
 
-        doc = Parser();
+        Document doc = Parser();
         Elements linkTags = doc.select("link");
-        List<Elements> listLinkTags = new ArrayList<Elements>();
+        List<Element> listLinkTags = new ArrayList<>();
 
-        for (Elements e : new ArrayList<Elements>(listLinkTags)){
+        for (Element e : linkTags){
 
-            listLinkTags.add(linkTags);
+            listLinkTags.add(e);
         }
         
 
@@ -159,21 +94,20 @@ public class DocParser
     /**
      * Extracts all script tags
      * 
-     * @param the parsed html file
      * 
      * @return a list of all script tags
      */
 
-    public List <Elements> extractScripts(Document doc) throws IOException{
+    public List <Element> extractScripts() throws IOException{
 
 
-         doc =  Parser();
+        Document doc =  Parser();
         Elements scriptTags = doc.select("script");
-        List<Elements> listScriptTags = new ArrayList<Elements>();
+        List<Element> listScriptTags = new ArrayList<>();
         
-        for (Elements e : new ArrayList<Elements>(listScriptTags)){
+        for (Element e : scriptTags){
 
-            listScriptTags.add(scriptTags);
+            listScriptTags.add(e);
         }
         
 
