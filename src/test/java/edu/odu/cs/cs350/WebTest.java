bf67680ae749f5d7efdb2411ec76287f18bbc705
rmack1020 <rmack005@odu.edu>
2022-08-03 22:01:51 -0400

    Webtest Unit Test
    GetDirectoryList Test

--- src/test/java/edu/odu/cs/cs350/WebTest.java
+++ src/test/java/edu/odu/cs/cs350/WebTest.java
@@ -1,12 +1,15 @@
 package edu.odu.cs.cs350;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
+import static org.junit.matchers.JUnitMatchers.*;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
 
 import org.junit.jupiter.api.Test;
 
@@ -37,7 +40,25 @@ public class WebTest{
 
         
     }
-
+    @Test
+    public void testGetFileList(){
+        List<Path> allFiles = new ArrayList<>();
+        List<Path> allDirectories = new ArrayList<>();
+        
+    
+    }
+    @Test
+    public void testGetDirectoryList()
+    {
+        List<Path> allDirectories = new ArrayList<>();
+        Path p1 = Paths.get("C:", "documents", "tests");
+        allDirectories.add(p1);
+        Website w = new Website(p1);
+        String Directory;
+        
+        Directory = w.getDirectoryList().toString();
+        assertThat(Directory, containsString("C:"));
+    }
 
    /*  @Test void testFindFilesInDir(){
         String path = "src/test/data/";
