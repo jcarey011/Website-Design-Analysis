2fe34b48b6f6a10d67bfb1afd838ac68d4419d97
Jasmine <jcare011@odu.edu>
2022-08-04 18:29:29 -0400

    removing commented out lines

--- src/test/java/edu/odu/cs/cs350/WebTest.java
+++ src/test/java/edu/odu/cs/cs350/WebTest.java
@@ -27,15 +27,6 @@ public class WebTest{
         
         
         
-        /*String [] thingy = new String[]{"1", "settings.ini", "src/test/data/Outline.html", "src/test/data/Outz.html"};
-        List<File> files = new ArrayList<>();
-        File f1 = new File("src/test/data/Outline.html");
-        File f2 = new File("src/test/data/Outz.html"); 
-        files.add(f1);
-        files.add(f2);
-
-        Website a = new Website(thingy);
-        assertEquals(files,a.getSourceFiles());*/
     }
 
     @Test
@@ -63,14 +54,6 @@ public class WebTest{
         assertThat(actual, is(expected));
     }
 
-   /*  @Test void testFindFilesInDir(){
-        String path = "src/test/data/";
-        File dir = new File(path);
-        Website input = new Website();
-        Options options = new Options();
-        List<File> files = input.findFilesInDir(dir, options);
-
-        //assertEquals(5, files.size());
-    }
-    */
+   
+    
 }
