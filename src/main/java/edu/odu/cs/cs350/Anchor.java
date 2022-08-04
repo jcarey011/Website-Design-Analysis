965b472b0a189dc019899ea23ab0721f64b01b76
Joel Lavinder <jdlavinder@gmail.com>
2022-08-04 15:42:47 -0400

    changed class name; began algorithm structure

--- src/main/java/edu/odu/cs/cs350/Anchor.java
+++ src/main/java/edu/odu/cs/cs350/Anchor.java
@@ -1,23 +1,32 @@
 package edu.odu.cs.cs350;
 
-import org.jsoup.nodes.*;
+import java.util.ArrayList;
+import java.util.List;
+import org.jsoup.nodes.Element;
 
-enum linkType {
-	intraPage, intraSite, External
-}
+	enum linkType {
+		intraPage, intraSite, External
+	}
 
 public class Anchor {
-	
+
 	linkType linktype;
-	
+
 	public Anchor(linkType linktype) {
 		this.linktype = linktype;
-		
-	}
-	
-	public void getLinkType (String args[]) {
-		Anchor test = new Anchor(linkType.intraPage);
 
+	}
+
+	public List<linkType> getLinkTypes (List<Element> tags) {
+        List<linkType> listLinkTypes = new ArrayList<>();
+		for (Element e  : tags){
+			// Need to design algorithm for determining intraPage (# at start or URL), intraSite (SameHost.com), External (DifferentHost.com)
+        }
+        return listLinkTypes;
+//		Temporarily for reference:        
+//		Anchor test = new Anchor(linkType.intraPage);
+//		return test;
+        
 	}
 
 }
